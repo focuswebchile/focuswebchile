4068684d538e79dbd801e216bc9f0d21fae12a15
Felipe Ibar <felipeibar@Felipes-Mac-Studio.local>
2026-02-04 15:53:38 +0100

    update footer links and process tweaks

--- src/components/home-sections-lazy.tsx
+++ src/components/home-sections-lazy.tsx
@@ -2,10 +2,13 @@
 
 import dynamic from "next/dynamic"
 
-const HomeSections = dynamic(() => import("@/components/home-sections"), {
+const HomeSections = dynamic(
+  () => import("@/components/home-sections").then((mod) => mod.HomeSections),
+  {
   ssr: false,
   loading: () => <div className="min-h-[40vh]" aria-hidden="true" />,
-})
+  }
+)
 
 export default function HomeSectionsLazy() {
   return <HomeSections />
